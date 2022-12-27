ba1cf4537c39235bf6797c4f4d4fba5960c98422
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-12-27 21:15:52 +0700

    style updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-let words = ['behance', 'beautiful'];
+let words = ['apple', 'snow', 'kraken'];
 
 const search = document.querySelector('.search');
 const wordList = document.querySelector('.word-list');
+
+words.forEach(word => wordList.innerHTML += `<div class='word'>${word}</div>`)
 search.addEventListener('keyup', () => {
     wordList.innerHTML = '';
     words.forEach((word) => {
@@ -9,10 +11,10 @@ search.addEventListener('keyup', () => {
         if (searchWord != -1) {
             wordList.innerHTML +=
                 `
-            <div class="word"><span> ${word.slice(0, searchWord)}</span><span style='color: red'>${search.value}</span><span>${word.slice(searchWord + search.value.length)}</span></div>
+            <div class="word"><span> ${word.slice(0, searchWord)}</span><span style='background-color:white; color:#5182FF'>${search.value}</span><span>${word.slice(searchWord + search.value.length)}</span></div>
             `;
         } else if (search.value == '') {
-            wordList.innerHTML += word;
+            wordList.innerHTML += `<div class='word'>${word}</div>`;
         }
     })
 })
