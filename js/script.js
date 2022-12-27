aed632e17e777b71b5f08898a1f5cbd8638ab0ef
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-12-27 21:43:51 +0700

    added new words and added action in empty search

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-let words = ['apple', 'snow', 'kraken'];
+let words = ['a', 'able', 'apple', 'above', 'across',
+    'australia', 'against', 'apartment', 'beautiful', 'bitch',
+    'business', 'become', 'between', 'breakfast',
+    'building', 'cabinet', 'cafeteria', 'captain',
+    'careful', 'century', 'children', 'cigarettes',
+    'climbed', 'combustible', 'confidential', 'continued',
+    'country', 'coupon', 'covered', 'daughter', 'decimal',
+    'destination', 'developed', 'difference', 'discovered',
+    'download', 'drawing', 'dress', 'each', 'edge', 'emergency',
+    'english', 'employer', 'everyone', 'everything', 'experiment',
+    'family', 'frontend', 'feeling', 'finished', 'firefighter', 'fuck',
+    'flammable', 'flowers', 'fraction', 'gentlemen', 'girl',
+    'government', 'grandparents', 'gamedev', 'ground', 'happened',
+    'hardware', 'here', 'history', 'hospital', 'however', 'important',
+    'information', 'instruments', 'interview', 'jumped', 'keyboard',
+    'language', 'laptop', 'laundry', 'length', 'machine', 'manager',
+    'message', 'middle', 'morning', 'mountain', 'northbound', 'newspapers',
+    'often', 'paragraph', 'passengers', 'pedestrians', 'perishable', 'president',
+    'probably', 'question', 'reached', 'receptionist', 'remember', 'reservations',
+    'restaurant', 'round', 'scientist', 'sentence', 'senior', 'shodon', 'shouted',
+    'skateboard', 'something', 'southbound', 'statement', 'students', 'supermarket',
+    'suppose', 'surprise', 'temperature', 'themselves', 'thousand', 'together', 'training',
+    'understand', 'usually', 'warning', 'website', 'whether', 'window', 'withdrawal', 'young',
+];
 
 const search = document.querySelector('.search');
 const wordList = document.querySelector('.word-list');
@@ -16,7 +39,11 @@ search.addEventListener('keyup', () => {
         } else if (search.value == '') {
             wordList.innerHTML += `<div class='word'>${word}</div>`;
         }
-    })
+    });
+
+    if (wordList.innerHTML == '') {
+        wordList.innerHTML = 'Ничего не найдено :(';
+    }
 })
 
 
